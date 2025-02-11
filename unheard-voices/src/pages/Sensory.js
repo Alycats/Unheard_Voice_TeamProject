8b2265167706afbdd598ec1680c3bf7138e1915f
dpataki <dpataki8@gmail.com>
2025-02-11 12:17:36 +0000

    Added some quiz questions to sensory page

--- unheard-voices/src/pages/Sensory.js
+++ unheard-voices/src/pages/Sensory.js
@@ -3,29 +3,29 @@ import Quiz from '../components/Quiz';
 
 const sensoryQuestions = [
   {
-    question: "Question Here",
-    options: ["Option A", "Option B", "Option C", "Option D"],
+    question: "What is the name given to a person who struggles with too much sensory input?",
+    options: ["Hyposensitive", "Hypersensitive", "Autistic", "Supersensitive"],
+    correctAnswer: "Hypersensitive",
+  },
+  {
+    question: "What is the name given to a person who struggles with not enough input?",
+    options: ["Hypersensitive", "Hyposensitive", "Autistic", "Subsensitive"],
+    correctAnswer: "Hyposensitive",
+  },
+  {
+    question: "Which of the following is a symptom of someone suffering from Hypersensitivity?",
+    options: ["Covering ears or eyes", "Jumping or spinning around", "Making lots of noise", "Touching lots of things"],
     correctAnswer: "Option A",
   },
   {
-    question: "Question Here",
-    options: ["Option A", "Option B", "Option C", "Option D"],
-    correctAnswer: "Option A",
+    question: "Which of the following is a symptom of someone suffering from Hyposensitivity",
+    options: ["Jumping or spinning around", "Minimising movement", "Covering ears or eyes", "avoiding touching anything"],
+    correctAnswer: "",
   },
   {
-    question: "Question Here",
-    options: ["Option A", "Option B", "Option C", "Option D"],
-    correctAnswer: "Option A",
-  },
-  {
-    question: "Question Here",
-    options: ["Option A", "Option B", "Option C", "Option D"],
-    correctAnswer: "Option A",
-  },
-  {
-    question: "Question Here",
-    options: ["Option A", "Option B", "Option C", "Option D"],
-    correctAnswer: "Option A",
+    question: "What is it called when someone suffering from hypersensitivity receives too much sensory input?",
+    options: ["Sensory Shutdown", "Sensory Eruption", "Sensory Overload", "Sensory Meltdown"],
+    correctAnswer: "Sensory Overload",
   },
   {
     question: "Question Here",
