a3f2dfe10f57be4cafe003d076a8875e0e9fd4d3
Luther Cole <LC1288@student.aru.ac.uk>
2025-02-27 15:59:36 +0000

    red dot

--- unheard-voices/src/App.js
+++ unheard-voices/src/App.js
@@ -1,14 +1,21 @@
 import './styles/App.css';  // Corrected path
 import Navbar from './components/Navbar'; 
 import { Outlet } from 'react-router-dom'; 
+import RedDot from './components/RedDot';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Outlet />
+      <RedDot />
     </div>
-  );
+);
 }
-
 export default App;
+
+
+
+
+
+
