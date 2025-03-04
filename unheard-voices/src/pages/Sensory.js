14d8c6fabca92dc0995c93205d08477626fbe8bb
jw1462 <jw1462@student.aru.ac.uk>
2025-03-04 12:03:37 +0000

    test

--- unheard-voices/src/pages/Sensory.js
+++ unheard-voices/src/pages/Sensory.js
@@ -56,8 +56,8 @@ const sensoryQuestions = [
 
 function Sensory() {
   return (
-    <div class="sensorymain">
-      <h1 class="sensoryheader">Sensory Challenges and Autism</h1>
+    <div>
+      <h1 style="background-color: red;">Sensory Challenges and Autism</h1>
       <p>Learn about the sensory processing challenges that many individuals with autism face and how to address them.</p>
 
       <p>Sensory Difficulties in people with autism fall into two categories: Hypersensitivity and Hyposensitivity</p>
